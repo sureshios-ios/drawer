367aeddc3607292bb7d3c65ef9518bb112a1440c
suresh <p.suresh2014@gmail.com>
2020-12-24 18:51:39 +0530

    Drawer

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+home: Scaffold(
+  appBar: AppBar(
+    title: Text('Drawer'),
+  ),
+  drawer: Drawer(
+    child: ListView(
+      children: <Widget> [
+        DrawerHeader(child: Text('his is drawer Header'),
+        ),
+        ListTile(
+          title: Text('This is Title 1'),
+        ),
+        ListTile(
+          title: Text('This is Title 1'),
+        ),
+        ListTile(
+          title: Text('This is Title 1'),
+        ),
+        ListTile(
+          title: Text('This is Title 1'),
+        ),
+      ],
+    ),
+  ),
+),
+    );
+  }
+}
